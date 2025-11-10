7a17887f468eb89bbe2457630e24bfca9316c7d3
myronKP <myronkp1@gmail.com>
2025-11-10 15:50:43 +0200

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-OPENAI_API_KEY = ("sk-proj-ncPrywq_gCfZbgDJivfYvgGRNCOZZ6oXmg4hbBXRjxfJMXt7NtUxkwTy6Q40d7CteoksnT3BlkfJDplXPZgx"
-                  "P6AmduISaMaC9F5jAvthpB4XpqlUbQE0F59GBR4RctcI5b5SwNdGz0BdBpylocA")
+OPENAI_API_KEY = ("")
+
 BOT_TOKEN      = "7237692096:AAGh_SBPZad4zJ-wi6XNp1-UCrGLd1INnn8"
